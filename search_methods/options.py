7fbe9eaeceb2f6e497231c79f060c878ce9c1485
georgeNakayama <w4756677@stanford.edu>
2021-04-30 11:23:10 -0700

    "compiled data gotten from kaggle dataset"

--- search_methods/options.py
+++ search_methods/options.py
@@ -1,10 +1,17 @@
 from utils.search_utils import create_options
-human_options = [["F1", "U1", "F-1"], ["F1", "B1", "F-1"]]
+human_options = [["R1", "U1", "R-1", "U-1"], ["L-1", "U-1", "L1", "U1"]]
 # 2-look OLL algorithms (for matching the third layer)
-str_OLLs = ["F R U R' U' F' f R U R' U' f'", "F R U R' U' F'", "f R U R' U' f'", "R U2 R' U' R U' R'", "R U R' U R U' R' U R U2 R'", "F R' F' r U R U' r'", "R U2 R2 U' R2 U' R2 U2 R", "R U R' U R U2 R'", "r U R' U' r' F R F'", "R2 D R' U2 R D' R' U2 R'"]
-str_PLLs = ["F R U' R' U' R U R' F' R U R' U' R' F R F'", "R U R' U' R' F R2 U' R' U' R U R' F'", "M2 U M2 U2 M2 U M2", "R U' R U R U R U' R' U' R2", "R2 U R U R' U' R' U' R' U R'", "M' U M2 U M2 U M' U2 M2"]
+str_OLLs = ["F R U R' U' F' B U L U' L' B'", "F R U R' U' F'", "B U L U' L' B'", "R U2 R' U' R U' R'", "R U R' U R U' R' U R U2 R'", "F R' F' L F R F' L'", "R U2 R2 U' R2 U' R2 U2 R", "R U R' U R U2 R'", "L F R F' L' F R F'", "R2 D R' U2 R D' R' U2 R'"]
+str_PLLs = ["F R U' R' U' R U R' F' R U R' U' R' F R F'", "R U R' U' R' F R2 U' R' U' R U R' F'", "M2 D M2 U2 M2 D M2", "R U' R U R U R U' R' U' R2", "R2 U R U R' U' R' U' R' U R'", "M2 D M2 U M' F2 M2 B2 M'"]
 def get_OLLs():
     return create_options(str_OLLs)
 def get_PLLs():
     return create_options(str_PLLs)
-print(get_PLLs()[2])
+
+
+
+def generate_options(human: bool) -> List[List[str]]:
+    options = human_options
+    if human: 
+        return human_options + get_OLLs() + get_PLLs()
+    else 
