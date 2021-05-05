ccb87b7bd6ed45bf53369fb03c0883ff7f401107
WenXinDong2018 <wxd@stanford.edu>
2021-05-05 20:51:35 +0800

    Update training script

--- ctg_approx/training_scripts.sh
+++ ctg_approx/training_scripts.sh
@@ -38,23 +38,23 @@ python search_methods/astar.py --states data/cube3/test/data_0.pkl --model saved
 python search_methods/astar.py --states data/cube3/test/data_0.pkl --model saved_models/cube3layer1_dynamic_difficulty_25_fixed_length/current/ --env cube3_layer1 --weight 0.6 --batch_size 1000 --results_dir results/cube3layer1_dynamic_difficulty_25_fixed_length/ --language python --nnet_batch_size 10000 --start_idx 900
 
 ###Layer 2
-#1. A* search without options using saved checkpoint of the layer 2 model. solve 100 cubes with first layer fixed 
+#1. First Layer Fixed: A* search without options using saved checkpoint of the layer 2 model. solve 100 cubes
 python search_methods/astar.py --states data/cube3_layer2/test/data_0.pkl --model saved_models/cube3layer2_dynamic_difficulty_25_fixed/current/ --env cube3_layer2 --weight 0.6 --batch_size 1000 --results_dir results/cube3layer2_dynamic_difficulty_25_fixed/fixed_layer_no_options --language python --nnet_batch_size 10000 --start_idx 900
 ##With Options
 python search_methods/astar.py --states data/cube3/test/data_0.pkl --model saved_models/cube3layer2_random_data_gen/current/ --env cube3_layer2 --weight 0.6 --batch_size 1000 --results_dir results/cube3layer2_dynamic_difficulty_25_fixed/fixed_layer_and_options --language python --nnet_batch_size 10000 --start_idx 900 --options --option_name layer2
 
-#2. A* search without options using saved checkpoint of the layer 2 model. solve 100 cubes with no fixed layers
+#2. No fixed layers:  A* search without options using saved checkpoint of the layer 2 model. solve 100 cubes
 python search_methods/astar.py --states data/cube3/test/data_0.pkl --model saved_models/cube3layer2_dynamic_difficulty_25_fixed/current/ --env cube3_layer2 --weight 0.6 --batch_size 1000 --results_dir results/cube3layer2_dynamic_difficulty_25_fixed/no_fixed_layer_no_options --language python --nnet_batch_size 10000 --start_idx 900
 ## With Options
 python search_methods/astar.py --states data/cube3/test/data_0.pkl --model saved_models/cube3layer2_dynamic_difficulty_25_fixed/current/ --env cube3_layer2 --weight 0.6 --batch_size 1000 --results_dir results/cube3layer2_dynamic_difficulty_25_fixed/no_fixed_layer_and_options --language python --nnet_batch_size 10000 --start_idx 900 --options --option_name layer2
 
 ###Layer 3
-#1. A* search without options using saved checkpoint of the layer 3 model. solve 100 cubes with first two layers fixed
+#1. First two layers fixed: A* search without options using saved checkpoint of the layer 3 model. solve 100 cubes
 python search_methods/astar.py --states data/cube3_layer3/test/data_0.pkl --model saved_models/cube3layer3_dynamic_difficulty_25_fixed_lt_03/current/ --env cube3_layer3 --weight 0.6 --batch_size 1000 --results_dir results/cube3layer3_dynamic_difficulty_25_fixed_lt_03/fixed_layer_no_options --language python --nnet_batch_size 10000 --start_idx 900
 #With Options
 python search_methods/astar.py --states data/cube3_layer3/test/data_0.pkl --model saved_models/cube3layer3_dynamic_difficulty_25_fixed_lt_03/current/ --env cube3_layer3 --weight 0.6 --batch_size 1000 --results_dir results/cube3layer3_dynamic_difficulty_25_fixed_lt_03/fixed_layer_and_options --language python --nnet_batch_size 10000 --start_idx 900 --options --option_name layer3
 
-#２. A* search without options using saved checkpoint of the layer 3 model. solve 100 cubes with no fixed layers
+#２. No fixed layers:: A* search without options using saved checkpoint of the layer 3 model. solve 100 cubes
 python search_methods/astar.py --states data/cube3/test/data_0.pkl --model saved_models/cube3layer3_dynamic_difficulty_25_fixed_lt_03/current/ --env cube3_layer3 --weight 0.6 --batch_size 1000 --results_dir results/cube3layer3_dynamic_difficulty_25_fixed_lt_03/no_fixed_layer_no_options --language python --nnet_batch_size 10000 --start_idx 900
 #With Options
 python search_methods/astar.py --states data/cube3/test/data_0.pkl --model saved_models/cube3layer3_dynamic_difficulty_25_fixed_lt_03/current/ --env cube3_layer3 --weight 0.6 --batch_size 1000 --results_dir results/cube3layer3_dynamic_difficulty_25_fixed_lt_03/no_fixed_layer_and_options --language python --nnet_batch_size 10000 --start_idx 900 --options --option_name layer3
@@ -63,7 +63,7 @@ python search_methods/astar.py --states data/cube3/test/data_0.pkl --model saved
 ###DeepCubeA
 #1. A* search without options using saved checkpoint of the cube3 model. solve 100 cubes
 python search_methods/astar.py --states data/cube3/test/data_0.pkl --model saved_models/cube3/current/ --env cube3 --weight 0.6 --batch_size 1000 --results_dir results/cube3_full/ --language python --nnet_batch_size 10000 --start_idx 900
-#2. A* search options using saved checkpoint of the cube3 model. solve 100 cubes
-python search_methods/astar.py --states data/cube3/test/data_0.pkl --model saved_models/cube3/current/ --env cube3 --weight 0.6 --batch_size 1000 --results_dir results/cube3_full/ --language python --nnet_batch_size 10000 --start_idx 900 --options
-
-
+#2. Save as above, no options, test on first two layers fixed
+python search_methods/astar.py --states data/cube3_layer3/test/data_0.pkl --model saved_models/cube3/current/ --env cube3 --weight 0.6 --batch_size 1000 --results_dir results/cube3_full_layer3/ --language python --nnet_batch_size 10000 --start_idx 900
+#3. A* search with options using saved checkpoint of the cube3 model. solve 100 cubes
+python search_methods/astar.py --states data/cube3/test/data_0.pkl --model saved_models/cube3/current/ --env cube3 --weight 0.6 --batch_size 1000 --results_dir results/cube3_full_options/ --language python --nnet_batch_size 10000 --start_idx 900 --options
